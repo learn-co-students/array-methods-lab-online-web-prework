6785906152ab421e000b6e1399e58f90100913d4
Darren L <darren.lou@gmail.com>
2019-02-20 01:18:33 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -11,13 +11,13 @@ def using_reverse(array)
 end
 
 def using_first(array)
-array.first
+  array.first
 end
 
 def using_last(array)
-array.last
+  array.last
 end
 
 def using_size(array)
-array.size
+  array.size
 end
