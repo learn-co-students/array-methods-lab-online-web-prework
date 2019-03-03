20882461d11b80f71b87577981c16a5e79ea3a1d
Patrick A <akolo@hotmail.com>
2019-03-03 19:47:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
 def using_include(array, element)
-
+  array.include?(element)
 end
 
 def using_sort(array)
-
+  famous_cats =["lil' bub", "grumpy cat", "maru"]
+  array = famous_cats
+  array.sort 
 end
 
 def using_reverse(array)
-
+  famous_wizards = ["Dumbledore", "Gandalf", "Merlin"]
+  array = famous_wizards
+  array.reverse 
 end
 
-def using_first(array)
-
+def using_first("lil' bub", "grumpy cat", "maru")
+    using_firt.first 
 end
 
-def using_last(array)
-
+def using_last("lil' bub", "grumpy cat", "maru")
+    using_last.last 
 end
 
-def using_size(array)
-
+def using_size("lil' bub", "grumpy cat", "maru")
+    using_size.size
 end
