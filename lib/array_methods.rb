21a398bbb90c402d0b7b88c2ccb7beade74fd8bf
Aaron Parkening <aaron.parkening@gmail.com>
2019-03-21 14:57:30 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,29 @@
+# Take arguments of an array and an element and return true if the array includes that element, false if it does not.
 def using_include(array, element)
-
+  array.include?(element)
 end
 
+# Take an argument of an array and return the sorted array.
 def using_sort(array)
-
+  array.sort
 end
 
+# Take an argument of an array and return that same array with the contents reversed.
 def using_reverse(array)
-
+  array.reverse 
 end
 
+# Take an argument of an array and return the first element in the array
 def using_first(array)
-
+  array.first
 end
 
+# Take an argument of an array and return the last element in the array
 def using_last(array)
-
+  array.last
 end
 
+# Take an argument of an array and return the size, or length, of the array
 def using_size(array)
-
+  array.length
 end
