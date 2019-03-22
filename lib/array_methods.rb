e4fd36d097b2fe3660052bf0eef596d729d5458b
Ryan <kerneltux0@gmail.com>
2019-03-22 12:41:28 -0600

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,29 +1,23 @@
 def using_include(array, element)
-  array = ["wow", "NYC", "Seattle", "London"]
   array.include?(element)
 end
 
 def using_sort(array)
-  array = ["wow", "NYC", "Seattle", "I"]
   array.sort
 end
 
 def using_reverse(array)
-  array = ["wow", "NYC", "Seattle", "arrays!"]
   array.reverse
 end
 
 def using_first(array)
-  array = ["wow", "NYC", "Seattle", "arrays!"]
   array.first
 end
 
 def using_last(array)
-  array = ["wow", "NYC", "Seattle", "arrays!"]
   array.last
 end
 
 def using_size(array)
-  array = ["wow", "NYC", "Seattle", "arrays!", "TARDIS", "sonic screwdriver"]
   array.size
 end
