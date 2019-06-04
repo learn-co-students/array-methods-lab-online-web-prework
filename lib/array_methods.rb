7be4e5c724c4e8b1022b3a3d0a2991870b5ce26d
KRIS DAVIS <krissdavis18@mail.com>
2019-06-04 07:16:05 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,23 +1,27 @@
-def using_include(array, element)
+
+  famous_cats = ["Hello Kitty", "Simba", "Heathcliff"]
+  famous_cats.include?("Garfield")
+  famous_cats.include?("Simba")
+end
+
+
+  famous_cats = ["Hello Kitty", "Simba", "Heathcliff"]
+  famous_cats.sort
+end
+
+  famous_men = ["Jordan", "Jackson", "Tyson"]
+  famous_men.revese
+end
+  
+  famous_men = ["Jordan", "Jackson", "Tyson"]
+  famous_men.first
 
 end
 
-def using_sort(array)
-
+  famous_men = ["Jordan", "Jackson", "Tyson"]
+  famous_men.last
 end
 
-def using_reverse(array)
-
-end
-
-def using_first(array)
-
-end
-
-def using_last(array)
-
-end
-
-def using_size(array)
-
+  famous_men = ["Jordan", "Jackson", "Tyson"]
+  famous_men.size
 end
